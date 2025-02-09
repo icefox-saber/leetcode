ca96a6e1fc7f1a3b513a875d42106e9ec6b25bef
icefox-saber <728862050a@gmail.com>
2025-02-09 12:03:50 +0800

    80

---
+++ two-pointers-medium/80.remove-duplicates-from-sorted-array-ii.cpp
@@ -0,0 +1,123 @@
+// @lcpr-before-debug-begin
+
+// @lcpr-before-debug-end
+
+/*
+ * @lc app=leetcode.cn id=80 lang=cpp
+ * @lcpr version=30204
+ *
+ * [80] Remove Duplicates from Sorted Array II
+ */
+
+// @lcpr-template-start
+#include <algorithm>
+#include <array>
+#include <atomic>
+#include <bitset>
+#include <cassert>
+#include <ccomplex>
+#include <cctype>
+#include <cerrno>
+#include <cfenv>
+#include <cfloat>
+#include <chrono>
+#include <cinttypes>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <complex>
+#include <condition_variable>
+#include <csetjmp>
+#include <csignal>
+#include <cstdalign>
+#include <cstdarg>
+#include <cstdbool>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctgmath>
+#include <ctime>
+#include <cwchar>
+#include <cwctype>
+#include <deque>
+#include <exception>
+#include <forward_list>
+#include <fstream>
+#include <functional>
+#include <future>
+#include <initializer_list>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <type_traits>
+#include <typeindex>
+#include <typeinfo>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+using namespace std;
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+  int removeDuplicates(vector<int> &nums) {
+    if (nums.size() < 3) {
+      return nums.size();
+    }
+
+    auto l = nums.begin() + 2, e = nums.end();
+    auto r = l;
+    for_each(r, e, [&](decltype(*r) n) {
+      if (n != *(l - 2)) {
+        swap(n, *l);
+        l++;
+      }
+    });
+
+    return l - nums.begin();
+  }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [1,1,1,1]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [0,0,1,1,2,2,3,3]\n
+// @lcpr case=end
+
+ */
