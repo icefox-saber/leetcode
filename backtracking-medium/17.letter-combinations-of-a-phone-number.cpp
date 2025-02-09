8cb54305d9f2b59cbdc510f109647b97a850bc30
icefox-saber <728862050a@gmail.com>
2025-02-09 22:03:49 +0800

    17

---
+++ backtracking-medium/17.letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,141 @@
+/*
+ * @lc app=leetcode.cn id=17 lang=cpp
+ * @lcpr version=30204
+ *
+ * [17] Letter Combinations of a Phone Number
+ */
+
+// @lcpr-template-start
+#include <algorithm>
+#include <array>
+#include <atomic>
+#include <bitset>
+#include <cassert>
+#include <ccomplex>
+#include <cctype>
+#include <cerrno>
+#include <cfenv>
+#include <cfloat>
+#include <chrono>
+#include <cinttypes>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <complex>
+#include <condition_variable>
+#include <csetjmp>
+#include <csignal>
+#include <cstdalign>
+#include <cstdarg>
+#include <cstdbool>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctgmath>
+#include <ctime>
+#include <cwchar>
+#include <cwctype>
+#include <deque>
+#include <exception>
+#include <forward_list>
+#include <fstream>
+#include <functional>
+#include <future>
+#include <initializer_list>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <type_traits>
+#include <typeindex>
+#include <typeinfo>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+using namespace std;
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+  void recurrence(const string::iterator &num, const string::iterator &end,
+                  const map<char, string> &table, vector<string> &res,
+                  string &s) {
+    if (num == end) {
+      res.push_back(s);
+      return;
+    }
+
+    for (const auto &ch : table.at(*num)) {
+      s.push_back(ch);
+      recurrence(num + 1, end, table, res, s);
+      s.pop_back();
+    }
+  }
+
+  vector<string> letterCombinations(string digits) {
+    if (digits.empty()) {
+      return vector<string>();
+    }
+
+    map<char, string> table;
+    table['2'] = "abc";
+    table['3'] = "def";
+    table['4'] = "ghi";
+    table['5'] = "jkl";
+    table['6'] = "mno";
+    table['7'] = "pqrs";
+    table['8'] = "tuv";
+    table['9'] = "wxyz";
+    vector<string> res;
+    string s;
+    recurrence(digits.begin(), digits.end(), table, res, s);
+    return res;
+  }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// "23"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// ""\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "2"\n
+// @lcpr case=end
+
+ */
