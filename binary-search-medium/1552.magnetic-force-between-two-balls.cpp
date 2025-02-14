1bb80d93a1b9de176da23b8238d53aaa5d4f9b30
icefox-saber <728862050a@gmail.com>
2025-02-14 10:51:35 +0800

    1552

---
+++ binary-search-medium/1552.magnetic-force-between-two-balls.cpp
@@ -0,0 +1,141 @@
+// @lcpr-before-debug-begin
+
+// @lcpr-before-debug-end
+
+/*
+ * @lc app=leetcode.cn id=1552 lang=cpp
+ * @lcpr version=30204
+ *
+ * [1552] Magnetic Force Between Two Balls
+ */
+
+// @lcpr-template-start
+#include <algorithm>
+#include <array>
+#include <atomic>
+#include <bitset>
+#include <cassert>
+#include <ccomplex>
+#include <cctype>
+#include <cerrno>
+#include <cfenv>
+#include <cfloat>
+#include <chrono>
+#include <cinttypes>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <complex>
+#include <condition_variable>
+#include <csetjmp>
+#include <csignal>
+#include <cstdalign>
+#include <cstdarg>
+#include <cstdbool>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctgmath>
+#include <ctime>
+#include <cwchar>
+#include <cwctype>
+#include <deque>
+#include <exception>
+#include <forward_list>
+#include <fstream>
+#include <functional>
+#include <future>
+#include <initializer_list>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <type_traits>
+#include <typeindex>
+#include <typeinfo>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+using namespace std;
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+  int maxDistance(vector<int> &position, int m) {
+    sort(position.begin(), position.end());
+    int n = position.back();
+
+    auto isvalid = [&n](int i) { return i <= n; };
+    auto isable = [&](int x) {
+      int last = position.front();
+      int num = 1;
+
+      for (const auto &posit : position) {
+        if ((posit - last) >= x) {
+          ++num;
+          last = posit;
+        }
+      }
+      return num >= m;
+    };
+    int l = 1, r = position.back();
+
+    while (l < r) {
+      int mid = (r + l) / 2 + 1;
+      if (isable(mid)) {
+        l = mid;
+      } else {
+        r = mid - 1;
+      }
+    }
+    return l;
+  }
+};
+// @lc code=end
+
+// @lcpr-div-debug-arg-start
+// funName=maxDistance
+// paramTypes= ["number[]","number"]
+// @lcpr-div-debug-arg-end
+
+/*
+// @lcpr case=start
+// [1,2,3,4,7]\n3\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [5,4,3,2,1,1000000000]\n2\n
+// @lcpr case=end
+
+ */
