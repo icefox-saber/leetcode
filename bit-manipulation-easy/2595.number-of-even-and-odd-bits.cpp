81e08d26b5869c6da306cc4aa0f3b4e25e8b71c4
icefox-saber <728862050a@gmail.com>
2025-02-20 11:07:06 +0800

    2595

---
+++ bit-manipulation-easy/2595.number-of-even-and-odd-bits.cpp
@@ -0,0 +1,118 @@
+/*
+ * @lc app=leetcode.cn id=2595 lang=cpp
+ * @lcpr version=30204
+ *
+ * [2595] Number of Even and Odd Bits
+ */
+
+// @lcpr-template-start
+#include <algorithm>
+#include <array>
+#include <atomic>
+#include <bitset>
+#include <cassert>
+#include <ccomplex>
+#include <cctype>
+#include <cerrno>
+#include <cfenv>
+#include <cfloat>
+#include <chrono>
+#include <cinttypes>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <complex>
+#include <condition_variable>
+#include <csetjmp>
+#include <csignal>
+#include <cstdalign>
+#include <cstdarg>
+#include <cstdbool>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctgmath>
+#include <ctime>
+#include <cwchar>
+#include <cwctype>
+#include <deque>
+#include <exception>
+#include <forward_list>
+#include <fstream>
+#include <functional>
+#include <future>
+#include <initializer_list>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <type_traits>
+#include <typeindex>
+#include <typeinfo>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+using namespace std;
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+  vector<int> evenOddBit(int n) {
+    int copy = n;
+    int odd = 0, even = 0;
+    while (n) {
+      even += n % 2;
+      n /= 4;
+    }
+    copy /= 2;
+    while (copy) {
+      odd += copy % 2;
+      copy /= 4;
+    }
+
+    return {even, odd};
+  }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// 50\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 2\n
+// @lcpr case=end
+
+ */
