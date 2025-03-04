ee20a3b226002e8533714d23d7e6c29ca7c06bf1
icefox-saber <728862050a@gmail.com>
2025-03-04 11:09:08 +0800

    1745

---
+++ dynamic-programming-hard/1745.palindrome-partitioning-iv.cpp
@@ -0,0 +1,125 @@
+/*
+ * @lc app=leetcode.cn id=1745 lang=cpp
+ * @lcpr version=30204
+ *
+ * [1745] Palindrome Partitioning IV
+ */
+
+// @lcpr-template-start
+#include <algorithm>
+#include <array>
+#include <atomic>
+#include <bitset>
+#include <cassert>
+#include <ccomplex>
+#include <cctype>
+#include <cerrno>
+#include <cfenv>
+#include <cfloat>
+#include <chrono>
+#include <cinttypes>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <complex>
+#include <condition_variable>
+#include <csetjmp>
+#include <csignal>
+#include <cstdalign>
+#include <cstdarg>
+#include <cstdbool>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctgmath>
+#include <ctime>
+#include <cwchar>
+#include <cwctype>
+#include <deque>
+#include <exception>
+#include <forward_list>
+#include <fstream>
+#include <functional>
+#include <future>
+#include <initializer_list>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <type_traits>
+#include <typeindex>
+#include <typeinfo>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+using namespace std;
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+  bool checkPartitioning(string s) {
+    vector dp(s.size(), vector(s.size(), true));
+
+    for (size_t len = 2; len <= s.size(); len++) {
+      for (size_t i = 0; i + len <= s.size(); i++) {
+        dp[i][i + len - 1] =
+            s[i] == s[i + len - 1] ? dp[i + 1][i + len - 2] : false;
+      }
+    }
+
+    for (size_t j = s.size() - 1; j > 1; j--) {
+      if (dp[j][s.size() - 1]) {
+        for (size_t i = j - 1; i > 0; i--) {
+          if (dp[0][i - 1] && dp[i][j - 1]) {
+            return true;
+          }
+        }
+      }
+    }
+
+    return false;
+  }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// "abcbdd"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "bcbddxy"\n
+// @lcpr case=end
+
+ */
