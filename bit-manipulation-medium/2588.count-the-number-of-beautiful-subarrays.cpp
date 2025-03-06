ff0297e67e01dd8da5c84f64e94c130462dc8bf6
icefox-saber <728862050a@gmail.com>
2025-03-06 21:06:18 +0800

    2588

---
+++ bit-manipulation-medium/2588.count-the-number-of-beautiful-subarrays.cpp
@@ -0,0 +1,115 @@
+/*
+ * @lc app=leetcode.cn id=2588 lang=cpp
+ * @lcpr version=30204
+ *
+ * [2588] Count the Number of Beautiful Subarrays
+ */
+
+// @lcpr-template-start
+#include <algorithm>
+#include <array>
+#include <atomic>
+#include <bitset>
+#include <cassert>
+#include <ccomplex>
+#include <cctype>
+#include <cerrno>
+#include <cfenv>
+#include <cfloat>
+#include <chrono>
+#include <cinttypes>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <complex>
+#include <condition_variable>
+#include <csetjmp>
+#include <csignal>
+#include <cstdalign>
+#include <cstdarg>
+#include <cstdbool>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctgmath>
+#include <ctime>
+#include <cwchar>
+#include <cwctype>
+#include <deque>
+#include <exception>
+#include <forward_list>
+#include <fstream>
+#include <functional>
+#include <future>
+#include <initializer_list>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <type_traits>
+#include <typeindex>
+#include <typeinfo>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+using namespace std;
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+  long long beautifulSubarrays(vector<int> &nums) {
+    unordered_map<int, int> cnt;
+    cnt[0] = 1;
+    long long res = 0;
+    int tmp = 0;
+    for (auto num : nums) {
+      tmp ^= num;
+      res += cnt[tmp]++;
+    }
+
+    return res;
+  }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [4,3,1,2,4]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,10,4]\n
+// @lcpr case=end
+
+ */
